3824f5675a46de9025479d0e04aac136f38f7cb4
Monishkumar12 <102236096+Monishkumar12@users.noreply.github.com>
2023-01-03 15:18:42 +0530

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ res.writeHead(200, {'Content-Type': 'text/html'});
 
 // Writing static text
 
-res.end('Hi Every-one Wish you a sparkling New Year! ...');
+res.end('Hi E-1 Wish you a sparkling New Year! ...');
 
 // Server listening on port number 10000
 
